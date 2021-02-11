c69b26c8474ee91051feb472fd3fd0e34a35b4a5
Issa <dsmzkali@yahoo.com>
2021-02-11 14:26:33 +0200

    add carousel elements and javascript file

---
+++ js/carousel.js
@@ -0,0 +1,8 @@
+const track = document.querySelector('.carousel_track');
+const buttons = document.querySelector('.carousel_button');
+
+let translateX = 0;
+
+buttons.forEach(button => {
+    button.addEventListener
+});
