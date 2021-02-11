5b0daacde8a0f76ce6a7d64bcd90d7dfea80775b
Issa <dsmzkali@yahoo.com>
2021-02-11 14:28:49 +0200

    scrub javascript file

--- js/carousel.js
+++
@@ -1,8 +0,0 @@
-const track = document.querySelector('.carousel_track');
-const buttons = document.querySelector('.carousel_button');
-
-let translateX = 0;
-
-buttons.forEach(button => {
-    button.addEventListener
-});
