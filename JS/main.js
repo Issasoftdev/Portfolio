e9ce6ff961ab2b5258e8a12b72563e582c505b23
Issa <dsmzkali@yahoo.com>
2021-05-28 17:46:13 +0200

    add navigation functionality

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,16 @@
- 
+const menuBtn = document.getElementById("menuBtn");
+const menuExitBtn = document.getElementById("menuExitBtn");
+const portfolioMenu = document.getElementById("portfolioMenu");
+
+function openMenu() {
+    portfolioMenu.style.display = "block";
+    menuBtn.style.display = "none";
+};
+
+function closeMenu() {
+    portfolioMenu.style.display = "none";
+    menuBtn.style.display = "block";
+};
+
+menuBtn.addEventListener('click', openMenu);
+menuExitBtn.addEventListener('click', closeMenu);
